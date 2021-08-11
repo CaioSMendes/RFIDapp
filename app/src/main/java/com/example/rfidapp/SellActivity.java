f4c31d1ecb0ce713b970f2f6517968d1d4870226
CaioSMendes <caioitallo.souzamendes@gmail.com>
2021-08-11 18:00:46 -0300

    ajustes imagens e cores

--- app/src/main/java/com/example/rfidapp/SellActivity.java
+++ app/src/main/java/com/example/rfidapp/SellActivity.java
@@ -9,5 +9,6 @@ public class SellActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell);
     }
 }
