c9501f10c877e1fbb9474a19db0f0de2682d738d
CaioSMendes <caioitallo.souzamendes@gmail.com>
2021-08-11 19:18:14 -0300

    add telas cadastro de categorias e fornecedores

--- app/src/main/java/com/example/rfidapp/CategorieActivity2.java
+++ app/src/main/java/com/example/rfidapp/CategorieActivity2.java
@@ -2,13 +2,38 @@ package com.example.rfidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CategorieActivity2 extends AppCompatActivity {
+
+    ImageButton categorieButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorie);
+
+        categorieButton = findViewById(R.id.buttonCadcategoria);
+
+        categorieButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CadcategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        categorieButton = findViewById(R.id.buttonListcategoria);
+
+        categorieButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ListcategoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/rfidapp/ProviderActivity.java
+++ app/src/main/java/com/example/rfidapp/ProviderActivity.java
@@ -2,13 +2,37 @@ package com.example.rfidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ProviderActivity extends AppCompatActivity {
+
+    ImageButton providerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider);
+
+        providerButton = findViewById(R.id.buttonCadfornecedor);
+
+        providerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CadproviderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        providerButton = findViewById(R.id.buttonListfornecedor);
+
+        providerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ListproviderActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
