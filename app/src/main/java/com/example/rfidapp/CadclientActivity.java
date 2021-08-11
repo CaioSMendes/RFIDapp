88306181d86f3b6ac761a98c9fc6326aeb1f7ecf
CaioSMendes <caioitallo.souzamendes@gmail.com>
2021-08-11 19:35:18 -0300

    add telas cadastro e list client

---
+++ app/src/main/java/com/example/rfidapp/CadclientActivity.java
@@ -0,0 +1,14 @@
+package com.example.rfidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadclientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadclient);
+    }
+}
--- app/src/main/java/com/example/rfidapp/ClientActivity.java
+++ app/src/main/java/com/example/rfidapp/ClientActivity.java
@@ -2,13 +2,36 @@ package com.example.rfidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ClientActivity extends AppCompatActivity {
+
+    ImageButton clientButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+
+        clientButton = findViewById(R.id.buttonCadcliente);
+
+        clientButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CadclientActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        clientButton = findViewById(R.id.buttonListclient);
+
+        clientButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ListclientActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
